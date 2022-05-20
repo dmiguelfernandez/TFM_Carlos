3e1d5abd5df957dce338a711b687be6f3ef30b3a
dmiguelfernandez <47295246+dmiguelfernandez@users.noreply.github.com>
2022-05-20 17:25:49 +0200

    Change npm

--- inicio.sh
+++ inicio.sh
@@ -3,9 +3,8 @@
 
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+npm install -g npm
+npm install -g node
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/api"
@@ -21,11 +20,6 @@ sudo chmod -R 777 /home/ec2-user/api
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/api
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
