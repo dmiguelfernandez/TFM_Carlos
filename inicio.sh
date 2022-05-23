b989947a3aa3b308c5ea2503f801236f38036f52
dmiguelfernandez <47295246+dmiguelfernandez@users.noreply.github.com>
2022-05-23 17:27:48 +0200

    DEPLOY

--- inicio.sh
+++ inicio.sh
@@ -1,31 +1,25 @@
-
 #!/bin/bash
 
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install nodejs
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/api"
+DIR="/home/api"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+  sudo mkdir ${DIR}
 fi
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/api
+sudo chmod -R 777 /home/api
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/api
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+cd /home/api
 
 #install node modules
 npm install
